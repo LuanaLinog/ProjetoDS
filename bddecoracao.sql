54f72f329b4ebae0541803d28a133541acdac6a7
LuanaLinog <Lua.linog@gmail.com>
2025-06-11 20:37:25 -0300

    PROJETO DS - VERSÃO 2.0
    Neste projeto consta:
    -Menu;
    -Cadastro de usuário;
    -Login de usuário;
    -Base de dados (Mysql).

--- bddecoracao.sql
+++ bddecoracao.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Maio-2025 às 13:28
+-- Tempo de geração: 06-Jun-2025 às 15:44
 -- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.0.25
+-- versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,6 +26,40 @@ USE `bddecoracao`;
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE `cliente` (
+  `cpf` varchar(14) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `endereco` varchar(40) NOT NULL,
+  `telefone` varchar(14) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `cidade` varchar(20) NOT NULL,
+  `rg` varchar(15) NOT NULL,
+  `estado` char(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+CREATE TABLE `produto` (
+  `codprod` int(11) NOT NULL,
+  `marca` varchar(20) NOT NULL,
+  `descricao` varchar(40) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `unMedida` varchar(10) NOT NULL,
+  `preco` double(6,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `usuario`
 --
 
@@ -40,10 +74,32 @@ CREATE TABLE `usuario` (
 --
 
 --
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- Índices para tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`codprod`);
+
+--
 -- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`login`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `codprod` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
